acb0c9588174f190d21cf5fa8ebaa2bcc640402d
JoeTuohy <joe.tuohy89@gmail.com>
2021-06-05 19:50:43 +0000

    added function to update the sales sheet

--- run.py
+++ run.py
@@ -29,19 +29,21 @@ def get_sales_data():
             print("Data is valid")
             break
 
-        return sales_data
+    return sales_data
 
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into ints. Rases ValueError if strings can't be converted to int or if there aren't exactly 6 values
+    Inside the try, converts all string values into ints.
+    Rases ValueError if strings can't be converted to int
+    or if there aren't exactly 6 values
     """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-            f"Exactly 6 values required, you provided {len(values)}"
-        )
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
         return False
@@ -49,4 +51,16 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet add new row with the list data provided
+    """
+    print("...updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
